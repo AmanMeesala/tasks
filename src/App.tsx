90c22c6fd7c713e7e1597ecb7ddd94751ef2f9fd
Aman Meesala <amanmees@udel.edu>
2026-02-11 14:02:07 -0500

    finished html and css task -3

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 
 function App(): React.JSX.Element {
     return (
@@ -10,13 +10,29 @@ function App(): React.JSX.Element {
                 Aman Meesala UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Hello World</p>
-            <img src="https://imgs.search.brave.com/BTKrPhmsFPavO4pAsHeQk3X2GB9Vf79xeJnD8Qs4Hxs/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI2/Mzg1MjQzNC9waG90/by9mcmVzaGx5LWJh/a2VkLWJyZWFkLW9u/ZS1vZi1saWZlcy1z/aW1wbGVzdC1wbGVh/c3VyZXMuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPVF5OW1x/N1hub2pGbjRTQWlO/R3N6NVZNcS1DelhK/VzJBc1lMWWVzUy1p/Rjg9"  />
+            <img src="https://imgs.search.brave.com/BTKrPhmsFPavO4pAsHeQk3X2GB9Vf79xeJnD8Qs4Hxs/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTI2/Mzg1MjQzNC9waG90/by9mcmVzaGx5LWJh/a2VkLWJyZWFkLW9u/ZS1vZi1saWZlcy1z/aW1wbGVzdC1wbGVh/c3VyZXMuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPVF5OW1x/N1hub2pGbjRTQWlO/R3N6NVZNcS1DelhK/VzJBc1lMWWVzUy1p/Rjg9"  alt= "bread alert"/>
             <p>Nicknames for Cash</p>
-                <ol> </ol>
+                <ol>
                     <li>Buns</li>
                     <li>Green</li>
                     <li>Dough</li>
-            <div><Button onClick={ () => { console.log("I am logged") } }>Click Me</Button></div>
+                     </ol>
+                    
+            <div><Button onClick={ () => { console.log("Hello World!") } }>Log Hello World</Button></div>
+         
+    <Container>
+            <Row>
+                <Col>First column.
+                <div style={ {backgroundColor: 'red', padding: '100px'} }>
+        
+                </div></Col>
+                <Col>
+                    Second column.
+         
+                    <div style={ {backgroundColor: 'red', padding: '8px'} }></div>
+                </Col>
+            </Row>
+        </Container>
             <p>
             
                 Edit <code>src/App.tsx</code> and save. This page will
