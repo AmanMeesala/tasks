bfa3e9af84c3f29e2a234b120e6832b15b5ad3b2
Aman Meesala <amanmees@udel.edu>
2026-03-08 15:36:14 -0400

    finished cycleholiday

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,43 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<string>("Christmas");
+
+    const emojis: Record<string, string> = {
+        Christmas: "🎄",
+        Halloween: "🎃",
+        Thanksgiving: "🦃",
+        Easter: "🐰",
+        "Valentine's Day": "❤️",
+    };
+
+    function nextAlphabet() {
+        setHoliday(
+            holiday === "Christmas" ? "Easter"
+            : holiday === "Easter" ? "Halloween"
+            : holiday === "Halloween" ? "Thanksgiving"
+            : holiday === "Thanksgiving" ? "Valentine's Day"
+            : "Christmas",
+        );
+    }
+
+    function nextYear() {
+        setHoliday(
+            holiday === "Valentine's Day" ? "Easter"
+            : holiday === "Easter" ? "Halloween"
+            : holiday === "Halloween" ? "Thanksgiving"
+            : holiday === "Thanksgiving" ? "Christmas"
+            : "Valentine's Day",
+        );
+    }
+
+    return (
+        <div>
+            <p>Holiday: {emojis[holiday]}</p>
+
+            <Button onClick={nextAlphabet}>Advance by Alphabet</Button>
+
+            <Button onClick={nextYear}>Advance by Year</Button>
+        </div>
+    );
 }
